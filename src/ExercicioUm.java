d1322917f180963e315eab5ba1b60e7387893f33
geraldoMatheus21 <geraldomatheus2@gmail.com>
2025-05-08 16:35:04 -0300

    Ex1

--- src/ExercicioUm.java
+++ src/ExercicioUm.java
@@ -3,13 +3,23 @@ import java.util.Random;
 public class ExercicioUm {
     public static void main(String[] args) {
        int [][] matriz = GeradodeMatriz(3, 4);
-
-       /** for (int i = 0; i < matriz.length; i++){
+       int [][] matriz2 = GeradodeMatriz(3,4);
+       int sem = 0;
+        System.out.println("Matriz 1 ------------------------------------");
+       for (int i = 0; i < matriz.length; i++){
             for(int j = 0; j < matriz[0].length; j++){
                 System.out.print(matriz[i][j] + "\t");
             }
             System.out.println();
         }
+        System.out.println("Matriz 2 -------------------------------------");
+        for (int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz2[0].length; j++){
+                System.out.print(matriz2[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        
         int pares = 0;
         for (int i = 0; i < matriz.length; i++){
             for(int j = 0; j < matriz[0].length; j++){
@@ -30,9 +40,17 @@ public class ExercicioUm {
             System.out.println();
 
         }
+        for(int i = 0; i<matriz.length;i++){
+            for(int j = 0; j < matriz2[0].length;j++){
+                if (matriz[i][j] == matriz2[i][j]) {
+                    sem++;
+                }
+            }
+        }
 
         System.out.println("Números pares: " + pares);
-        System.out.println("Maior número: " + maior); **/
+        System.out.println("Maior número: " + maior);
+        System.out.println("Numeros semelhantes entre as matrizes: " + sem);
     }
     static int [][] GeradodeMatriz(int N, int M){
         Random g = new Random();
